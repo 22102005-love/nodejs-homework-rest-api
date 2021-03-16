cf0f5832b6fc8154f591ecb9b6f03ff447e1aafb
Tatiana Rak <raktania3@gmail.com>
2021-03-16 22:20:24 +0200

    upload/update avatars and update fields of user

--- model/schemas/user.js
+++ model/schemas/user.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcryptjs");
+const gravatar=require('gravatar')
 const { Subscription } = require("../../helpers/constants");
 const SALT_WORK_FACTOR=8
 
@@ -16,6 +17,12 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: [true, "Password required"],
+  },
+  avatarURL: {
+    type: String,
+    default: function () {
+      return gravatar.url(this.email,{s:'250'},true)
+    }
   },
   subscription: {
     type: String,
--- routes/api/users/validation.js
+++ routes/api/users/validation.js
@@ -1,4 +1,5 @@
 const Joi = require('joi')
+const {HttpCode}= require('../../../helpers/constants')
 
 const schemaCreateUser = Joi.object({
     email: Joi.string().required(),
@@ -7,7 +8,7 @@ const schemaCreateUser = Joi.object({
 
 const schemaLoginUser = Joi.object({
     email: Joi.string().required(),
-    password:Joi.string().required(),
+    password: Joi.string().required(),
 })
 
 const validate = (schema, obj, next) => {
@@ -26,4 +27,15 @@ module.exports.createUser = (req, res,next) => {
 }
 module.exports.loginUser = (req, res,next) => {
      return validate(schemaLoginUser,req.body,next)
+}
+module.exports.validateUploadAvatar = (req, res, next) => {
+    if (!req.file) {
+        return res.status(HttpCode.BAD_REQUEST).join({
+            status: 'error',
+            code: HttpCode.BAD_REQUEST,
+            data: 'Bad request',
+            message:'field of avatar with file not found',
+        })
+    }
+    next()
 }
